835719ee0e87a0b4e1d39fa8f9e9d79297dd999d
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-07-06 10:52:03 -0300

    feat: init register form

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  cadastroForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl(''),
+      cpf: new FormControl(''),
+      email: new FormControl(''),
+      senha: new FormControl(''),
+      senhaConfirmacao: new FormControl('')
+    })
+  }
+
+  adicionarUsuario() {
+    let user = this.cadastroForm.value;
+    console.log(user);
   }
 
 }
