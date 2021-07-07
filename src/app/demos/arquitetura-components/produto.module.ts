1d549dbfb6d3d99a6be4a1bbbf51912ee3f9089d
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-07-07 16:36:48 -0300

    feat: @input decorator

--- src/app/demos/arquitetura-components/produto.module.ts
+++ src/app/demos/arquitetura-components/produto.module.ts
@@ -1,11 +1,18 @@
 import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { ProdutoRoutingModule } from './produto.route';
+
+import localePt from '@angular/common/locales/pt';
+import { ProdutoDetalheComponent } from './components/produto-card-detalhe.component';
+import { ProdutoCountComponent } from './components/produto-count.component';
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
-    ProdutoDashboardComponent
+    ProdutoDashboardComponent,
+    ProdutoDetalheComponent,
+    ProdutoCountComponent
   ],
   imports: [
     CommonModule,
