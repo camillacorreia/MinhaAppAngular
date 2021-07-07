23cf79ef71b18289feffd9d8eb7caf1aea77da35
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-07-07 18:12:44 -0300

    feat: @viewchildren decorators

--- src/app/demos/arquitetura-components/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-components/produto-dashboard/produto-dashboard.component.ts
@@ -1,14 +1,19 @@
 import { Produto } from './../models/produto';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import { ProdutoCountComponent } from '../components/produto-count.component';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html',
   styles: []
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
 
   produtos: Produto[];
+
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef;
+  @ViewChild(ProdutoCountComponent, { static: false }) contador: ProdutoCountComponent;
 
   constructor() { }
 
@@ -61,6 +66,18 @@ export class ProdutoDashboardComponent implements OnInit {
 
   mudarStatus(product: Produto){
     product.ativo = !product.ativo;
+  }
+
+
+  ngAfterViewInit(): void {
+    console.log('Objeto do Contador: ', this.contador.produtos);
+
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+    
+    clickTexto.subscribe(() => {
+      alert('clicou no texto!');
+      return;
+    })
   }
 
 }
